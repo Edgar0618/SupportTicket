8c56fd89a77709d2a76d9e15e373939bcf76f3f4
Edgar0618 <edgarguerrero591@gmail.com>
2025-05-15 22:29:04 -0400

    Taking a break

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,13 @@
 const express = require('express');
-require('dotenv').config();
+const dotenv = require('dotenv').config();
 const PORT = process.env.PORT || 4000;
 
 const app = express();
 
-app.get('/test', (req, res) => {
-  res.status(200).json({ message: 'Welcome to the Support Desk API'})
-});
+app.get('/', (req, res) => {
+  res.status(200).json({ message: 'Welcome to the Support Desk API' });
+})
 
-app.listen(PORT, () => { console.log(`Server started on port ${PORT}`); });
+app.use('/api/users', require('./routes/userRoutes'));
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
