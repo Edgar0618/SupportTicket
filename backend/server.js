f840593955887120ce135ede669237a83b6bc67f
Edgar0618 <edgarguerrero591@gmail.com>
2025-05-15 21:25:46 -0400

    Second day

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,11 @@
-console.log('Server...')
+const express = require('express');
+require('dotenv').config();
+const PORT = process.env.PORT || 4000;
+
+const app = express();
+
+app.get('/test', (req, res) => {
+  res.status(200).json({ message: 'Welcome to the Support Desk API'})
+});
+
+app.listen(PORT, () => { console.log(`Server started on port ${PORT}`); });
