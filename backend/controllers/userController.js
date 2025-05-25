5b57cde594ffc653c61ee419c5a33ae153310ee0
Edgar0618 <edgarguerrero591@gmail.com>
2025-05-25 19:23:12 -0400

    Error and Exception Handling

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,13 +1,28 @@
-const registerUser = (req, res) => {
+// @des     Register a new user
+// @route   /api/users
+// @access  Public
+const registerUser = asyncHandler(async (req, res) => {
+    const {name, email, password} = req.body
+
+    // Validation
+    // 400 client error
+    // 200 is OK
+    if(!name || !email || !password) {
+        res.status(400)
+        throw new Error("Please include all fields")
+    }
+
     res.send('Register Route')
-}
+})
 
-const loginUser = (req, res) => {
+// @des     Login a new user
+// @route   /api/users/login
+// @access  Public
+const loginUser = asyncHandler(async (req, res) => {
     res.send('Login Route')
-}
+})
 
-//Always have to export these
-
+//Always have to export these functions
 module.exports = {
     registerUser,
     loginUser
--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const dotenv = require('dotenv').config();
+const {errorHandler} = require('./middleware/errorMIddleware')
+const asyncHandler = require('express-async-handler')
 const PORT = process.env.PORT || 4000;
 
 const app = express();
+
+// middleware necessary to populate req.body properly in express routes
+// allow us to send raw JSON 
+// handling incoming request data/ json payloads (data inside)
+app.use(express.json())
+// handles form submissions
+// parses incoming requests with URL-encoded payloads
+app.use(express.urlencoded({extended: false}))
 
 app.get('/', (req, res) => {
   res.status(200).json({ message: 'Welcome to the Support Desk API' });
@@ -10,5 +20,9 @@ app.get('/', (req, res) => {
 
 // Routes
 app.use('/api/users', require('./routes/userRoutes'));
+
+app.use(errorHandler)
+
+app.use(asyncHandler)
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
