7b5cd9239c3ce708b5b0082a880d0d3613616af5
Edgar0618 <edgarguerrero591@gmail.com>
2025-10-14 16:39:53 -0400

    Add more debugging to see MONGO_URI value

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose')
 const connectDB = async() => {
     try {
         console.log('MONGO_URI from env:', process.env.MONGO_URI ? 'SET' : 'NOT SET')
+        console.log('MONGO_URI value:', process.env.MONGO_URI)
         const conn = await mongoose.connect(process.env.MONGO_URI)
         console.log(`MongoDB Connected to host: ${conn.connection.host}`.cyan.underline);
         console.log(`Using database name: ${conn.connection.name}`.magenta.bold);
